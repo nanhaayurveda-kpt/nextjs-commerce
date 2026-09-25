955abe1f18f09cb7ad03add0f9e66c73632a73ab
kp1153 <nanhaayurveda@gmail.com>
2026-09-25 22:05:02 +0530

    Fix: remove force-dynamic for cacheComponents compatibility

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,8 +7,6 @@ type Route = {
   lastModified: string;
 };
 
-export const dynamic = "force-dynamic";
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   validateEnvironmentVariables();
 
