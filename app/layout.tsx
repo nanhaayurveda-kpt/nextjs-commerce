f520dcbfcae8e35928537a95a6ac64d11793d0f1
kp1153 <nanhaayurveda@gmail.com>
2026-09-25 22:24:19 +0530

    Wrap Navbar in Suspense to fix prerender blocking on _not-found

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "components/layout/navbar";
 import { WelcomeToast } from "components/welcome-toast";
 import { GeistSans } from "geist/font/sans";
 import { getCart } from "lib/shopify";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import { Toaster } from "sonner";
 import "./globals.css";
 import { baseUrl } from "lib/utils";
@@ -34,7 +34,9 @@ export default async function RootLayout({
     <html lang="en" className={GeistSans.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
         <CartProvider cartPromise={cart}>
-          <Navbar />
+          <Suspense fallback={<div className="h-[65px]" />}>
+            <Navbar />
+          </Suspense>
           <main>
             {children}
             <Toaster closeButton />
